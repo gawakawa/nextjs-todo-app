064b1493f6f7d4b50bc4851d533b4dfe4ed3d93a
gawakawa <hirakuhasegawa@gmail.com>
2024-12-30 16:41:12 +0900

    :tada: Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,8 +28,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/new">New</Link>
+      </nav>
         {children}
       </body>
     </html>
   );
 }
+
